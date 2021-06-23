8efa26253621d64e2ea65f9c765e8ea22c32adbc
muiceking <scm1400@gmail.com>
2021-06-23 14:55:47 +0900

    비밀지도 Not Easy

---
+++ Lv1/비밀지도/ModelAnswer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String makeSharp(int n, int m) {
+        if(n == 0) {
+            if( m > 0) {
+                String str = "";
+                for(int i = 0; i < m; i++) {
+                    str += " ";
+                }
+                return str;
+            }
+            else return "";
+        }
+        else {
+            return n % 2 == 0 ? makeSharp(n/2, m-1) + " " : makeSharp(n/2, m-1) + "#"; 
+        }
+    }
+    public String[] solution(int n, int [] arr1, int [] arr2) {
+        String [] answer = new String[n];
+        int [] secretMap = new int[n];
+        for(int i = 0; i < n; i++) {
+            secretMap[i] = arr1[i] | arr2[i];
+            answer[i] = makeSharp(secretMap[i], n);
+        }
+        return answer;
+    }
+}
