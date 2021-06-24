6c940487d81ff54c29a0cd003c920e4150c74d79
muiceking <scm1400@gmail.com>
2021-06-24 14:47:54 +0900

    Comaparator 사용법 숙지

---
+++ Lv1/문자열 내 마음대로 정렬하기/ModelAnswer.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+       
+        
+        Arrays.sort(strings, new Comparator<String>() 
+        {
+        	@Override
+        	public int compare(String o1, String o2)
+        	{
+        		return Character.compare(o1.charAt(n), o2.charAt(n)) == 0 ? o1.compareTo(o2) : o1.charAt(n) - o2.charAt(n);
+        	}
+        }
+        );
+        
+        return strings;
+ 
+    }
+}
