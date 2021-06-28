20f6569f0c8646ab70ee974ed378e4ab0da2eb64
muiceking <scm1400@gmail.com>
2021-06-28 18:29:54 +0900

    no message

--- Lv1/이상한 문자 만들기/MyAnswer.java
+++ Lv1/이상한 문자 만들기/MyAnswer.java
@@ -4,12 +4,13 @@ class Solution {
         int temp = 2;
         for(int i = 0; i < s.length(); i++)
         {
-            if(s.charAt(i) == ' ') temp = 1;
+        	char text = s.charAt(i);
+            if(text == ' ') temp = 1;
             
             if(temp % 2 ==0)
-                answer += Character.toUpperCase(s.charAt(i));
+                answer += Character.toUpperCase(text);
             else
-                answer += Character.toLowerCase(s.charAt(i));
+                answer += Character.toLowerCase(text);
             temp ++;
         }
         return answer;
