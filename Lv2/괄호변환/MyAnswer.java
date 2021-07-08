ec027dc0f7e14352f670b7ba8a2186a7de5288e9
muiceking <scm1400@gmail.com>
2021-07-08 23:54:32 +0900

    어려웠음

--- Lv2/괄호변환/MyAnswer.java
+++ Lv2/괄호변환/MyAnswer.java
@@ -1,37 +1,59 @@
+import java.util.Stack;
+
 class Solution {
+    int at = 0;
+
     public String solution(String p) {
         String answer = "";
-
-        int count_l = 0;
-        int count_r = 0;
-
-        String u = "";
-        String v = "";
         if (p.equals(""))
-            return answer;
+            return p;
 
-        for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) == ')') {
-                count_r++;
+        boolean check = check(p);
+
+        String u = p.substring(0, at);
+        String v = p.substring(at);
+
+        if (check == true) {
+            return u + solution(v);
+        }
+
+        answer = "(" + solution(v) + ")";
+
+        for (int i = 1; i < u.length() - 1; i++) {
+            if (u.charAt(i) == '(') {
+                answer += ")";
             } else {
-                count_l++;
-            }
-            if (count_r == count_l) {
-                u = p.substring(0, i);
-                v = p.substring(i);
+                answer += "(";
             }
         }
-        System.out.println(u + " : " + v);
+
         return answer;
     }
 
     public boolean check(String p) {
+        boolean bool = true;
+        int count_l = 0;
+        int count_r = 0;
+        Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < p.length(); i++) {
-            if (!(p.startsWith("(") && p.endsWith(")") && p.length() % 2 == 0)) {
-                return false;
+            if (p.charAt(i) == '(') {
+                count_l++;
+                stack.push('(');
             } else {
-                check(p.substring(i + 1, p.length()));
+                count_r++;
+                if (stack.isEmpty()) {
+                    bool = false;
+                } else {
+                    stack.pop();
+                }
+            }
+
+            if (count_r == count_l) {
+                at = i + 1;
+                return bool;
             }
         }
+        return true;
     }
 }
